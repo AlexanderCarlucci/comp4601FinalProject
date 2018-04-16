fd10b0b5e3c2a7b47265a531806ee652629bb1b6
Alex Carlucci <alexc@ca.ibm.com>
2018-04-15 22:52:44 -0400

    kmeans and raw analysis

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 
 const express = require('express');
+const associate = require('./lib/associate.js');
 const setup = require('./lib/setup.js');
 const users = require('./lib/users.js');
+const algorith = require('./lib/algorithm');
 const cluster = require('./lib/kmeans.js');
 const dust = require('express-dustjs');
 const pagerank = require('pagerank-js');
 const path = require('path');
 
 
+
 const app = express();
 setup.configureUsers();
-cluster.cluster();
+const userList = cluster.cluster();
+const associations = associate(userList);
+console.log(associations);
 
 app.engine('dust', dust.engine({
   useHelpers: true
@@ -20,6 +25,9 @@ app.set('view engine', 'dust');
 app.set('views', path.resolve(__dirname, './views'));
 
 app.get("/start", setup.configureUsers);
+
+app.get('/user/reccomment/:userid', algorithm.do)
+
 
 app.get("/", users.renderAllUsers);
 
